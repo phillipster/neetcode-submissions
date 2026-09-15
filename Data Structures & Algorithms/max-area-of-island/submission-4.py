6b67c39d5764507a3412719a6db135eebd983434
Felipe <lipe.feldman@gmail.com>
2026-09-15 16:23:48 -0400

    Add: max-area-of-island - submission-4

---
+++ Data Structures & Algorithms/max-area-of-island/submission-4.py
@@ -0,0 +1,15 @@
+from collections import deque
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def dfs(i, j):
+            if not((0 <= i < len(grid)) and (0 <= j < len(grid[0]))) or grid[i][j] == 0:
+                return 0
+            grid[i][j] = 0
+            return 1 + dfs(i-1, j) + dfs(i, j-1) + dfs(i+1, j) + dfs(i, j+1)
+        
+        result = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    result = max(result, dfs(i, j))
+        return result
